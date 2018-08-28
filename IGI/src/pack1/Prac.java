1d0d5a538036568405041f3a330c36b3263f24ab
Shekhar <Shekhar@192.168.100.6>
2018-08-28 16:45:10 +0530

    Test Upload

---
+++ IGI/src/pack1/Prac.java
@@ -0,0 +1,56 @@
+package pack1;
+
+public class Prac {
+
+	public static void main(String[] args)
+	{
+		/*//converting string to integer
+		String someString = "123";
+		int i = Integer.parseInt( someString );
+		System.out.println(i);
+		
+		//type casting
+		double d = 52.8;
+		int i1 = (int) d;
+		System.out.println(i1);
+		
+		byte b = (byte) 1000.0;
+		System.out.println(b);
+		
+		byte b1 = 10;
+		double o = b1;
+		System.out.println(o);
+		
+		double d1 = 5;
+		System.out.println(d1);
+		
+		//boolean operations
+		boolean x = false;
+		System.out.println("x is "+x);
+		
+		x = true;
+		System.out.println("x is "+x);
+		
+		if(x) System.out.println("executed");
+		x = false;
+		if(x) System.out.println("not executed");
+				
+		System.out.println("10>9 is "+(10>9));
+		
+		//Data types
+		float f = 2.5f;
+		System.out.println(f);
+		
+		short s = 1000;
+		System.out.println(s);*/
+		
+		for(int i=0; i<=10;i++)
+		{
+			System.out.print(i+" ");
+			if(i%2 == 0) continue;
+			System.out.println();
+		}
+		
+	}
+
+}
